78afc20e2697554a89a202baea10f5cbb365dc1d
Rcparsln811 <receparslan811@gmail.com>
2024-05-05 17:40:12 +0300

    Header Update
    Logo and some title added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './css/Header.css';
+import cddy from './images/yuvarlak.png'
 
 function Header() {
     return (
         <div className='header' >
 
+            <img src={cddy} style={{ width: '100px', paddingLeft: '40px' }} />
             <div className='title'>CedidiyeFifa</div>
-
-
+            <a className='instatitle' href='https://www.instagram.com/cddyfifa/' >CddyFifa</a>
         </div>
     )
 }
